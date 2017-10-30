3271f9ba597e5b4dc7435be6d5ab5a4c110cdf64
Ana TSI <aasins@tsiberia.es>
2017-10-30 13:41:16 +0100

    nombre de las paginas en el menu y cosas del css

--- operativa/index.php
+++ operativa/index.php
@@ -54,7 +54,7 @@ if (isset($_SESSION['usuario'])==false) {
       <nav class="menu">
         <a href="index.php">Inicio</a>
         <a href="nuevoServicio.php">Nueva Actividad</a>
-        <a href="modificarServicio.php">Modificar Actividad</a>
+        <a href="modificarServicio.php">Actividades actuales</a>
         <a href="#">Histórico Actividades</a>
       </nav>
 
