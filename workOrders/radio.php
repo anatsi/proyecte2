27789657187ac3bd1f65abeb4fb15e80d9175990
Ana Asins <aasins@tsiberia.es>
2018-03-27 15:24:34 +0200

    **** tabla/excel/buscar de rol radio en la intranet

---
+++ workOrders/radio.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Clase encargada de las consultas a la tabla radio.
+ */
+
+ //Llamamos a la clase db, encargada de la conexion.
+ require_once 'dbJockeys.php';
+
+class Radio extends dbJockeys
+{
+  //la funcion construct llama al construct de db, encargada de la conexión.
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  function listaRadio(){
+    //Construimos la consulta
+    $sql="SELECT * FROM radio ORDER BY id DESC";
+    //Realizamos la consulta
+    $resultado=$this->realizarConsulta($sql);
+    if($resultado!=null){
+      //Montamos la tabla de resultados
+      $tabla=[];
+      while($fila=$resultado->fetch_assoc()){
+        $tabla[]=$fila;
+      }
+      return $tabla;
+    }else{
+      return null;
+    }
+  }
+
+  function listaRadioFiltrados($b)
+  {
+    //Contruimos la consulta.
+    $sql = "SELECT * FROM radio WHERE concat(bastidor, radio, clima, fecha, hora, usuario) LIKE '%".$b."%' ORDER BY id DESC";
+    //realizamos la consulta.
+    $resultado=$this->realizarConsulta($sql);
+    if ($resultado!=null) {
+      //Montamos la tabla de resultados
+      $tabla=[];
+      while ($fila=$resultado->fetch_assoc()) {
+        $tabla[]=$fila;
+      }
+      return $tabla;
+    }else {
+      return null;
+    }
+  }
+
+}
+ ?>
--- workOrders/registroRoles.php
+++ workOrders/registroRoles.php
@@ -54,6 +54,7 @@ if (isset($_SESSION['usuario'])==false) {
         <a href="registroCampa.php">Campa</a>
         <a href="registroWrap.php">Wrap Guard</a>
         <a href="filtroRoles.php">Roles</a>
+        <a href="registroRadio.php">Inspección radio/climatizador</a>
       </nav>
 
     </header>
