83477f3a49bbf16413e5b05b1a5580c30e139a98
Ana Asins <aasins@tsiberia.es>
2018-07-24 15:50:51 +0200

    **** Correo al finalizar la actividad (RRHH, Supervisores, Pilar)

--- operativa/operativa/finalizarServicio.php
+++ operativa/operativa/finalizarServicio.php
@@ -149,6 +149,42 @@ textarea::placeholder{
     }else {
       $finalizado=$servicio->FinalizarActividad($_POST['fin'], $_POST['fecha'], $_POST['comentario']);
       if ($finalizado == true) {
+        //enviamos un correo avisando de que se ha finalizado la actividad
+        //transformamos la fecha
+        $fecha=explode("-", $_POST['fecha']);
+        $fecha=$fecha[2]."-".$fecha[1]."-".$fecha[0];
+        //sacar la información de la actividad
+        $actividad = $servicio -> ServicioId($_POST['fin']);
+        //si sale bien, enviamos un correo a los responsables para avisarles.
+        // Enviar el email
+        $mail = "robot@tsiberia.es";
+
+        $header = 'From: ' . $mail . " \r\n";
+        $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+        $header .= "Mime-Version: 1.0 \r\n";
+        //$header .= "Content-Type: text/plain";
+        $header .= "Content-Type: text/html; charset=utf-8";
+
+        $mensaje = '<html>' . '<head><title>Email</title>
+        <style type="text/css">
+        h2 {
+            color: black;
+            font-family: Impact;
+          }
+        </style>
+        </head>' .
+        '<body>
+          <h4>
+            <b>La actividad "'.$actividad['descripcion'].'" finaliza el dia '.$fecha.'</b>
+          </h4><br />' .
+          'Por favor, no responda a este correo lo envia un robot automáticamente.'.
+          '<br />Enviado el ' . date('d/m/Y', time()) .
+        '</body></html>';
+
+        $para = 'aasins@tsiberia.es';
+        $asunto = 'Fin de la actividad "'.$actividad['descripcion'].'"';
+
+        mail($para, $asunto, $mensaje, $header);
         ?>
           <script type="text/javascript">
             window.location='actividadesActuales.php';
